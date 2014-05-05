6b5f2dab8237f7a9d3713cb4844b168340590783
bryanxoxo <bmason@engineyard.com>
2014-05-05 15:20:40 -0700

    first commit

---
+++ public/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="Bryan's page.css">
+</head>
+<body>
+<div id="First">
+	<br>
+	<p><img src="http://www.kraftfoodservice.com/ProductsandBrands/Sauces/PublishingImages/tapatioPageHeader.jpg"></p>
+	<br>
+	<br>
+	<p>My Name is Bryan. <a href="mailto:bryan.mason@me.com">AMA</a></p>
+	
+	<p>Here are some pictures I like.</p>
+	<br>
+	<br>
+	<p><Img src="http://static.giantbomb.com/uploads/original/12/122040/1826688-dinowar_1.jpg"></p>
+		<br>
+	<p><img src="http://distilleryimage2.ak.instagram.com/36b66e7e492011e3962312f43f382231_8.jpg"></p>
+</div>
+</body>
+</html>
