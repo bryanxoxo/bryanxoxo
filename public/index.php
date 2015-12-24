c917910990da80826bae2a3e6fc71c1e53d38ac0
bryanxoxo <bmason@engineyard.com>
2015-12-24 08:27:53 -0800

    updated the site to include a new picture of Kylo Ren and text

--- public/index.php
+++ public/index.php
@@ -11,10 +11,10 @@
 	<br>
 	<p>My Name is Bryan. <a href="mailto:bryan.mason@me.com">AMA</a></p>
 	
-	<p>Here are some pictures I like.</p>
+	<p>Here are some pictures I like. I'm a pretty big Star Wars fan. Like a lot of us Trekkies out there. TFA was a great reintroduction to the Universe so I decided to put a cool picture of Kylo Ren here. I may update this in the future to include more information regarding Star Wars! You can find me on Battlefront as well. Hit up the xoxo.</p>
 	<br>
 	<br>
-	<p><Img src="http://static.giantbomb.com/uploads/original/12/122040/1826688-dinowar_1.jpg"></p>
+	<p><Img src="https://www.washingtonpost.com/news/comic-riffs/wp-content/uploads/sites/15/2015/12/STARWARS-kylo-ren-awakens.jpg"></p>
 		<br>
 		<p>This is a picture of my dog Fry! He's pretty sweet.</p>
 	<p><img src="https://igcdn-photos-d-a.akamaihd.net/hphotos-ak-xfp1/t51.2885-15/10424621_515260315267315_1016508489_n.jpg"</p>
